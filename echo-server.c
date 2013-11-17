627572f3a455dd1829bffc59475df669b8d37799
John Szakmeister <john@szakmeister.net>
2013-11-17 06:08:13 -0500

    Refactor forking implementation to prepare for threading.

--- echo-server.c
+++ echo-server.c
@@ -60,6 +60,88 @@ sig_chld(int sig)
 #endif
 
 
+static void
+handle_client(int client_fd)
+{
+    char buffer[128];
+    ssize_t size, sent_size;
+
+    for (;;)
+    {
+        size = recv(client_fd, buffer, sizeof(buffer), 0);
+
+        // Handle errors.
+        if (size == -1)
+        {
+#ifdef ENABLE_ALARM
+            if (errno == EINTR)
+                continue;
+#endif
+
+            die_errno(errno);
+        }
+
+        // OS is saying the other side was closed.
+        if (size == 0)
+            break;
+
+        printf("Recv'd %ld bytes:\n<<<<<<\n", size);
+        write(1, buffer, size);
+        printf(">>>>>>\n");
+
+        char *p = buffer;
+
+        while (size)
+        {
+            sent_size = send(client_fd, p, size, 0);
+            if (sent_size == -1)
+            {
+#ifdef ENABLE_ALARM
+                if (errno == EINTR)
+                    continue;
+#endif
+
+                die_errno(errno);
+            }
+
+            p += sent_size;
+            size -= sent_size;
+        }
+    }
+
+    close(client_fd);
+
+    printf("Client disconnected\n");
+}
+
+
+#ifdef ENABLE_FORKING
+static void
+start_fork(int server_fd, int client_fd)
+{
+    pid_t pid;
+
+    /* Spawn off a new process to handle the client. */
+    pid = fork();
+
+    if (pid == 0)
+    {
+        /* This is the child. */
+        close(server_fd);
+
+        handle_client(client_fd);
+
+        exit(0);
+    }
+    else
+    {
+        /* Server doesn't need this. */
+        close(client_fd);
+    }
+}
+#endif
+
+
 /** @brief Main program entry point.
     @param[in] argc  Number of arguments in @c argv.
     @param[in] argv  Command-line arguments.
@@ -72,7 +154,6 @@ main(int argc, char *argv[])
     (void) argc;
     (void) argv;
 
-    char buffer[128];
     int server_fd = socket(PF_INET, SOCK_STREAM, 0);
 
     if (server_fd < 0)
@@ -124,9 +205,6 @@ main(int argc, char *argv[])
         struct sockaddr_in client;
         int client_fd;
         socklen_t sl = sizeof(client);
-#ifdef ENABLE_FORKING
-        pid_t pid;
-#endif
 
         client_fd = accept(server_fd, (struct sockaddr *) &client, &sl);
 
@@ -144,76 +222,14 @@ main(int argc, char *argv[])
         s_connection_count++;
 #endif
 
-        ssize_t size, sent_size;
-
         char *client_ip = inet_ntoa(client.sin_addr);
 
         printf("Connection received from: %s\n", client_ip);
 
-#ifdef ENABLE_FORKING
-        /* Spawn off a new process to handle the client. */
-        pid = fork();
-
-        if (pid == 0)
-        {
-            /* This is the child. */
-            close(server_fd);
-#endif
-            for (;;)
-            {
-                size = recv(client_fd, buffer, sizeof(buffer), 0);
-
-                // Handle errors.
-                if (size == -1)
-                {
-#ifdef ENABLE_ALARM
-                    if (errno == EINTR)
-                        continue;
-#endif
-
-                    die_errno(errno);
-                }
-
-                // OS is saying the other side was closed.
-                if (size == 0)
-                    break;
-
-                printf("Recv'd %ld bytes:\n<<<<<<\n", size);
-                write(1, buffer, size);
-                printf(">>>>>>\n");
-
-                char *p = buffer;
-
-                while (size)
-                {
-                    sent_size = send(client_fd, p, size, 0);
-                    if (sent_size == -1)
-                    {
-#ifdef ENABLE_ALARM
-                        if (errno == EINTR)
-                            continue;
-#endif
-
-                        die_errno(errno);
-                    }
-
-                    p += sent_size;
-                    size -= sent_size;
-                }
-            }
-
-            close(client_fd);
-
-            printf("Client disconnected\n");
-
-#ifdef ENABLE_FORKING
-            exit(0);
-        }
-        else
-        {
-            /* Server doesn't need this. */
-            close(client_fd);
-        }
+#if defined(ENABLE_FORKING)
+        start_fork(server_fd, client_fd);
+#else
+        handle_client(client_fd);
 #endif
     }
 
